3f31ecc29d27356796e21779b5559acecf0671f8
Nympheo <synthetic.probe@gmail.com>
2017-10-18 23:33:26 +0300

    login updated

--- src/Login.js
+++ src/Login.js
@@ -5,8 +5,16 @@ class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      email: '',
+      password: '',
+      polyclinic: ''
     };
+  }
+
+  signUp() {
+    console.log('signUp');
+    console.log('this.state', this.state);
+
   }
 
   render() {
@@ -18,20 +26,24 @@ class Login extends React.Component {
             className='form-control'
             type='email'
             placeholder='электронная почта'
+            onChange={event => this.setState({email: event.target.value})}
           />
           <input
             className='form-control'
             type='text'
             placeholder='пароль'
+            onChange={event => this.setState({password: event.target.value})}
           />
           <input
             className='form-control'
             type='text'
             placeholder='поликлиника'
+            onChange={event => this.setState({polyclinic: event.target.value})}
           />
           <button
             className='btn log-button'
-            type='button'>
+            type='button'
+            onClick={() => this.signUp()}>
               ВОЙТИ
           </button>
         </div>
